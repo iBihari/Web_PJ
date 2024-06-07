f34fef4ef4491fbf15881a3d0fe6ef7f1fca592c
Leminh0512 <info.minhle2003@gmail.com>
2024-06-08 01:49:33 +0700

    login

--- JS/script.js
+++ JS/script.js
@@ -1,4 +1,15 @@
 let videoBtn = document.querySelectorAll('.vid-btn');
+let formBtn = document.querySelector('#login-btn');
+ let loginForm = document.querySelector('.login-form-container');
+let formClose = document.querySelector('#form-close');
+
+formBtn.addEventListener('click', () =>{
+  loginForm.classList.add('active');
+});
+
+formClose.addEventListener('click', () =>{
+  loginForm.classList.remove('active');
+});
 
 videoBtn.forEach(btn =>{
     btn.addEventListener('click', ()=>{
