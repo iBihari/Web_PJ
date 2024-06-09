f930c16c0078c5e16fb9df3967b793f8a743ea03
iBihari <ngnhubac41113@gmail.com>
2024-06-10 04:46:56 +0700

    js cho phần dịch vụ khách sạn

--- JS/khachsangr.js
+++ JS/khachsangr.js
@@ -3,9 +3,18 @@ function selecthn(){
     var hpclick=document.getElementById("hp");
     var hdclick=document.getElementById("hd");
 
+
     hnclick.classList.add("hnclicked");
     hpclick.classList.remove("hpclicked");
     hdclick.classList.remove("hdclicked");
+
+    var ehanoi = document.getElementById("hanoi");
+    var ehaiphong = document.getElementById("haiphong");
+    var ehaiduong = document.getElementById("haiduong");
+    ehanoi.classList.remove("none")
+    ehaiphong.classList.add("none"); 
+    ehaiduong.classList.add("none"); 
+
 }
 function selecthp(){
     var hpclick=document.getElementById("hp");
@@ -15,13 +24,41 @@ function selecthp(){
     hpclick.classList.add("hpclicked");
     hnclick.classList.remove("hnclicked");
     hdclick.classList.remove("hdclicked");
+
+    var ehanoi = document.getElementById("hanoi");
+    var ehaiphong = document.getElementById("haiphong");
+    var ehaiduong = document.getElementById("haiduong");
+    ehanoi.classList.add("none")
+    ehaiphong.classList.remove("none"); 
+    ehaiduong.classList.add("none"); 
 }
 function selecthd(){
     var hdclick=document.getElementById("hd");
     var hnclick=document.getElementById("hn");
     var hpclick=document.getElementById("hp");
 
-    hdclick.classList.add("hdclicked");
+    hdclick.classList.remove("hdclicked");
     hpclick.classList.remove("hpclicked");
-    hnclick.classList.remove("hnclicked");
+    hnclick.classList.add("hnclicked");
+
+    var ehanoi = document.getElementById("hanoi");
+    var ehaiphong = document.getElementById("haiphong");
+    var ehaiduong = document.getElementById("haiduong");
+    ehanoi.classList.add("none")
+    ehaiphong.classList.add("none"); 
+    ehaiduong.classList.remove("none"); 
+}
+
+function removeClas(id, className) {
+    const element = document.getElementById(id);
+    
+    if (element && element.classList.contains(className)) {
+        
+        element.remove();
+    }
+}
+function addClas(id, className) {
+    var element = document.getElementById(id);
+    element.classList.add(className);   
+
 }
